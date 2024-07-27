de8547f4f41d934a5637caea366a4593d5018e6f
Sandeep-913 <vsaisandeep04@gmail.com>
2024-07-27 13:19:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,173 @@
+function myfunc1(){
+    const a1 = document.getElementById("p6d1").style.display;
+    if( a1 == "block"){
+        document.getElementById("p6d1").style.display = "none";
+        return
+    }
+    const a2 = document.getElementById("p6d2").style.display;
+    if( a2 == "block"){
+        document.getElementById("p6d2").style.display = "none";
+    }
+    const a3 = document.getElementById("p6d3").style.display;
+    if( a3 == "block"){
+        document.getElementById("p6d3").style.display = "none";
+    }
+    const a4 = document.getElementById("p6d4").style.display;
+    if( a4 == "block"){
+        document.getElementById("p6d4").style.display = "none";
+    }
+    const a5 = document.getElementById("p6d5").style.display;
+    if( a5 == "block"){
+        document.getElementById("p6d5").style.display = "none"
+    }
+    const a6 = document.getElementById("p6d6").style.display;
+    if( a6 == "block"){
+        document.getElementById("p6d6").style.display = "none";
+    }
+    document.getElementById("p6d1").style.display = "block";
+}
+
+function myfunc2(){
+    const a1 = document.getElementById("p6d1").style.display;
+    if( a1 == "block"){
+        document.getElementById("p6d1").style.display = "none";
+    }
+    const a2 = document.getElementById("p6d2").style.display;
+    if( a2 == "block"){
+        document.getElementById("p6d2").style.display = "none";
+        return
+    }
+    const a3 = document.getElementById("p6d3").style.display;
+    if( a3 == "block"){
+        document.getElementById("p6d3").style.display = "none";
+    }
+    const a4 = document.getElementById("p6d4").style.display;
+    if( a4 == "block"){
+        document.getElementById("p6d4").style.display = "none";
+    }
+    const a5 = document.getElementById("p6d5").style.display;
+    if( a5 == "block"){
+        document.getElementById("p6d5").style.display = "none"
+    }
+    const a6 = document.getElementById("p6d6").style.display;
+    if( a6 == "block"){
+        document.getElementById("p6d6").style.display = "none";
+    }
+    document.getElementById("p6d2").style.display = "block";
+}
+
+function myfunc3(){
+    const a1 = document.getElementById("p6d1").style.display;
+    if( a1 == "block"){
+        document.getElementById("p6d1").style.display = "none";
+    }
+    const a2 = document.getElementById("p6d2").style.display;
+    if( a2 == "block"){
+        document.getElementById("p6d2").style.display = "none";
+    }
+    const a3 = document.getElementById("p6d3").style.display;
+    if( a3 == "block"){
+        document.getElementById("p6d3").style.display = "none";
+        return
+    }
+    const a4 = document.getElementById("p6d4").style.display;
+    if( a4 == "block"){
+        document.getElementById("p6d4").style.display = "none";
+    }
+    const a5 = document.getElementById("p6d5").style.display;
+    if( a5 == "block"){
+        document.getElementById("p6d5").style.display = "none"
+    }
+    const a6 = document.getElementById("p6d6").style.display;
+    if( a6 == "block"){
+        document.getElementById("p6d6").style.display = "none";
+    }
+    document.getElementById("p6d3").style.display = "block";
+}
+
+function myfunc4(){
+    const a1 = document.getElementById("p6d1").style.display;
+    if( a1 == "block"){
+        document.getElementById("p6d1").style.display = "none";
+    }
+    const a2 = document.getElementById("p6d2").style.display;
+    if( a2 == "block"){
+        document.getElementById("p6d2").style.display = "none";
+    }
+    const a3 = document.getElementById("p6d3").style.display;
+    if( a3 == "block"){
+        document.getElementById("p6d3").style.display = "none";
+    }
+    const a4 = document.getElementById("p6d4").style.display;
+    if( a4 == "block"){
+        document.getElementById("p6d4").style.display = "none";
+        return
+    }
+    const a5 = document.getElementById("p6d5").style.display;
+    if( a5 == "block"){
+        document.getElementById("p6d5").style.display = "none"
+    }
+    const a6 = document.getElementById("p6d6").style.display;
+    if( a6 == "block"){
+        document.getElementById("p6d6").style.display = "none";
+    }
+    document.getElementById("p6d4").style.display = "block";
+}
+
+function myfunc5(){
+    const a1 = document.getElementById("p6d1").style.display;
+    if( a1 == "block"){
+        document.getElementById("p6d1").style.display = "none";
+    }
+    const a2 = document.getElementById("p6d2").style.display;
+    if( a2 == "block"){
+        document.getElementById("p6d2").style.display = "none";
+    }
+    const a3 = document.getElementById("p6d3").style.display;
+    if( a3 == "block"){
+        document.getElementById("p6d3").style.display = "none";
+    }
+    const a4 = document.getElementById("p6d4").style.display;
+    if( a4 == "block"){
+        document.getElementById("p6d4").style.display = "none";
+    }
+    const a5 = document.getElementById("p6d5").style.display;
+    if( a5 == "block"){
+        document.getElementById("p6d5").style.display = "none"
+        return
+    }
+    const a6 = document.getElementById("p6d6").style.display;
+    if( a6 == "block"){
+        document.getElementById("p6d6").style.display = "none";
+    }
+    document.getElementById("p6d5").style.display = "block";
+}
+
+function myfunc6(){
+    const a1 = document.getElementById("p6d1").style.display;
+    if( a1 == "block"){
+        document.getElementById("p6d1").style.display = "none";
+    }
+    const a2 = document.getElementById("p6d2").style.display;
+    if( a2 == "block"){
+        document.getElementById("p6d2").style.display = "none";
+    }
+    const a3 = document.getElementById("p6d3").style.display;
+    if( a3 == "block"){
+        document.getElementById("p6d3").style.display = "none";
+    }
+    const a4 = document.getElementById("p6d4").style.display;
+    if( a4 == "block"){
+        document.getElementById("p6d4").style.display = "none";
+    }
+    const a5 = document.getElementById("p6d5").style.display;
+    if( a5 == "block"){
+        document.getElementById("p6d5").style.display = "none"
+    }
+    const a6 = document.getElementById("p6d6").style.display;
+    if( a6 == "block"){
+        document.getElementById("p6d6").style.display = "none";
+        return
+    }
+    document.getElementById("p6d6").style.display = "block";
+}
